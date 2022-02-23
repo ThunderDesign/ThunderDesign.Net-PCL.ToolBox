11c943cc9eb1ce028c168cbbc318b86ccfb0c076
Shawn LaMountain <slamountain@gmail.com>
2022-02-22 20:23:14 -0500

    Initial commit

---
+++ src/ThunderDesign.Net-PCL.ToolBox/Extentions/PropertyInfoExtentions.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace ThunderDesign.Net.ToolBox.Extentions
+{
+    public static class PropertyInfoExtentions
+    {
+        public static bool CanCopyPropertyValue(this PropertyInfo self, PropertyInfo source)
+        {
+            if (source == null || self == null)
+                return false;
+            if (source.PropertyType != self.PropertyType || !source.CanRead || !self.CanWrite)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ src/ThunderDesign.Net-PCL.ToolBox/Extentions/StringExtentions.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ThunderDesign.Net.ToolBox.Extentions
+{
+    public static class StringExtentions
+    {
+        public static Stream AsStream(this string s)
+        {
+            return s.AsStream(Encoding.UTF8);
+        }
+
+        public static Stream AsStream(this string s, Encoding encoding)
+        {
+            return new MemoryStream(encoding.GetBytes(s ?? ""));
+        }
+    }
+}
